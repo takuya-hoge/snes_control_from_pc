3f0e04b3be802d72d096ca911d1ca225eca04a3c
takuya-hoge <quartz44kh@gmail.com>
2020-09-21 17:47:09 +0900

    Add PC test program

---
+++ ClassLibraryForPC/ClassLibraryForPC/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace snesControlFromPc
+{
+    public class snesPad
+    {
+        // parameter
+        public SerialPort serialPort;   // you can change settings.
+        public int pressTime = 70;     // default time;
+
+        public snesPad(string portNamem)
+        {
+            serialPort = new SerialPort(portNamem);
+            serialPort.BaudRate = 9600;
+        }
+
+        public bool open()
+        {
+            bool ret = true;
+
+            try
+            {
+                serialPort.Open();
+            }
+            catch
+            {
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        // this define is input for arduino.
+        public const byte BTN_B_ON = 1;
+        public const byte BTN_Y_ON = 2;
+        public const byte BTN_SELECT_ON = 3;
+        public const byte BTN_START_ON = 4;
+        public const byte BTN_UP_ON = 5;
+        public const byte BTN_DOWN_ON = 6;
+        public const byte BTN_LEFT_ON = 7;
+        public const byte BTN_RIGHT_ON = 8;
+        public const byte BTN_A_ON = 9;
+        public const byte BTN_X_ON = 10;
+        public const byte BTN_L_ON = 11;
+        public const byte BTN_R_ON = 12;
+        public const byte BTN_B_OFF = 13;
+        public const byte BTN_Y_OFF = 14;
+        public const byte BTN_SELECT_OFF = 15;
+        public const byte BTN_START_OFF = 16;
+        public const byte BTN_UP_OFF = 17;
+        public const byte BTN_DOWN_OFF = 18;
+        public const byte BTN_LEFT_OFF = 19;
+        public const byte BTN_RIGHT_ = 20;
+        public const byte BTN_A_OFF = 21;
+        public const byte BTN_X_OFF = 22;
+        public const byte BTN_L_OFF = 23;
+        public const byte BTN_R_OFF = 24;
+
+        public void send(byte input)
+        {
+            byte[] data = { input, (byte)(input+12) };
+            serialPort.Write(data, 0, 1);// ON
+            System.Threading.Thread.Sleep(pressTime);
+            serialPort.Write(data, 1, 1); // OFF
+        }
+
+    }
+}
---
+++ ClassLibraryForPC/testProgram/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using snesControlFromPc;
+using System.ComponentModel;
+
+namespace testProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            randomSends player_1 = new randomSends("COM4");
+            randomSends player_2 = new randomSends("COM10");
+
+            // Using super street fighter 2.
+            // Program starts when RYU appears.
+            Console.WriteLine("Send Start");
+            player_1.pad.send(snesPad.BTN_START_ON); // Start
+            System.Threading.Thread.Sleep(1500);
+            Console.WriteLine("Send Start");
+            player_1.pad.send(snesPad.BTN_START_ON); // Start
+            System.Threading.Thread.Sleep(700);
+            Console.WriteLine("Send Down");
+            player_1.pad.send(snesPad.BTN_DOWN_ON); // Down
+            System.Threading.Thread.Sleep(700);
+            Console.WriteLine("Send Start");
+            player_1.pad.send(snesPad.BTN_START_ON); // Start
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("RYU!");
+            player_1.pad.send(snesPad.BTN_A_ON); // RYU
+            Console.WriteLine("KEN!");
+            player_2.pad.send(snesPad.BTN_A_ON); // KEN
+            System.Threading.Thread.Sleep(4000);
+            Console.WriteLine("Send Start");
+            player_1.pad.send(snesPad.BTN_START_ON); // Start
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Send Start");
+            player_1.pad.send(snesPad.BTN_START_ON); // Start
+
+            // randomSends
+            player_1.start();
+            player_2.start();
+
+            Console.Write("**");
+            int origRow = Console.CursorTop;
+            int origCol = Console.CursorLeft;
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                Console.SetCursorPosition(origCol, origRow);
+                Console.Write("*");
+                System.Threading.Thread.Sleep(1000);
+                Console.SetCursorPosition(origCol, origRow);
+                Console.Write("/");
+            }
+        }
+    }
+
+    class randomSends
+    {
+        private BackgroundWorker worker;
+        public snesPad pad;
+
+        public randomSends(string port)
+        {
+            worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+
+            pad = new snesPad(port);
+            pad.open();
+        }
+
+        public void start()
+        {
+            worker.RunWorkerAsync();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            System.Random rand = new System.Random(20200921);
+            byte data;
+
+            while(true)
+            {
+                data = (byte)rand.Next(1, 12);
+                if (data != snesPad.BTN_START_ON)
+                {
+                    pad.send(data);
+                    System.Threading.Thread.Sleep(20);
+                }
+            }
+        }
+    }
+}
